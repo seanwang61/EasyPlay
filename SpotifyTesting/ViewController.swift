a79118b4e6985cf854745ac66e210f7e8754c6e8
Sean Wang <seanwang61@gmail.com>
2015-10-22 12:28:31 -0700

    Initial Commit

---
+++ SpotifyTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyTesting
+//
+//  Created by Sean Wang on 10/22/15.
+//  Copyright © 2015 cse190. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
